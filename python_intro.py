ae86e06dd3e48ce10d9ac4341e39d0ff5a013305
Katharina Hoven <katharina.brossok@web.de>
2023-12-16 15:12:58 +0100

    Meine Django-Girls-App, erster Commit

---
+++ python_intro.py
@@ -0,0 +1,8 @@
+def hallo(name):
+    print('Hallo ' + name + '!')
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'du']
+for name in girls:
+    hallo(name)
+    print('Nächstes Mädchen')
+for i in range(1, 6):
+    print(i)
